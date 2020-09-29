015dcb12fa7c47e9e6c5d6aa0f28e212f76f3481
tripathyA5 <69359952+tripathyA5@users.noreply.github.com>
2020-09-28 20:20:31 -0400

    Add files via upload

---
+++ box.js
@@ -0,0 +1,21 @@
+class Block {
+    constructor(x, y, width, height){
+      var options={
+          isStatic:false,
+
+          
+      }
+      this.body=Bodies.rectangle(x,y,width,height,options);
+      World.add(world,this.body);
+      this.height=height;
+      this.width=width;
+    }
+  display(){
+    push();
+      rectMode(CENTER);
+      rect(this.body.position.x,this.body.position.y,this.width,this.height)
+      this.visibilty=5;
+      pop();
+  }
+  }
+  
